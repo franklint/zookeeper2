6c7f6db825d68695079edf921d5ce2c65c16daf5
franklinT <franklintsung3@gmail.com>
2022-05-12 08:34:00 -0400

    clear message

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express'); 
+const {animals} = require('./data/animals.json') 
+
+
+
+const PORT = process.env.PORT || 3001; 
+const app = express(); 
+
+function filteredByQuery(query, animalsArray){
+    let personalityTraitsArray = []; 
+    let filteredResults = animalsArray; 
+    if(query.personalityTraits){
+        if(typeof query.personalityTraits === 'string'){
+            personalityTraitsArray = [query.personalityTraits]
+        } else {
+            personalityTraitsArray = query.personalityTraits; 
+        } 
+        //loop through each trait in the personalityTraits array: 
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        }); 
+    }
+
+    if(query.diet){
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species){
+        filteredResults = filteredResults.filter(animal => animal.species === query.species); 
+    }
+    if(query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name); 
+    }
+    return filteredResults; 
+}
+
+app.get('/api/animals' , (req, res)=>{
+    let results = animals; 
+    if(req.query){
+        results = filteredByQuery(req.query, results)
+    }
+    // console.log(req.query)
+    res.json(results);
+}) 
+
+
+app.listen(PORT, ()=> {
+    console.log(`API server now on port ${PORT}`); 
+}); 
